dd6e1c38648b4a6677d61c6e6f3fe334c1795821
Sandip123samanta <samanthasandip565@gmail.com>
2026-02-20 22:54:15 +0530

    chore: updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    outputFileTracingRoot: require('path').join(__dirname, '../../'),
+  },
 };
 
 export default nextConfig;
